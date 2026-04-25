8f8368d7476f378f6ea6a26ecb1cbd06ed847a57
Eberhardsteiner <Eberhard.steiner@uvm-institut.de>
2026-04-25 12:47:23 +0200

    Added index.html

---
+++ dist/assets/UnternehmenPage-xmoQLT-4.js
@@ -0,0 +1,1 @@
+import{j as e,U as n}from"./index-D_gVRqEn.js";import{H as t,F as s}from"./Footer-I-1cxkmy.js";import"./react-vendor-B711IIKO.js";import"./lucide-vendor-CEMe7T-P.js";function o(){return e.jsxs("div",{className:"min-h-screen bg-white",children:[e.jsx(t,{}),e.jsx("main",{id:"main-content",className:"pt-16 md:pt-20",children:e.jsx(n,{})}),e.jsx(s,{})]})}export{o as default};
