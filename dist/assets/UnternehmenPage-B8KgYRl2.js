0ab09ae1ab3827181d993298e23da4b45870d8f3
Eberhardsteiner <Eberhard.steiner@uvm-institut.de>
2026-04-25 12:47:20 +0200

    Deleted index.html

--- dist/assets/UnternehmenPage-B8KgYRl2.js
+++
@@ -1,1 +0,0 @@
-import{j as e,U as n}from"./index-C1b5SbPv.js";import{H as t,F as s}from"./Footer-chYELkB-.js";import"./react-vendor-B711IIKO.js";import"./lucide-vendor-CEMe7T-P.js";function o(){return e.jsxs("div",{className:"min-h-screen bg-white",children:[e.jsx(t,{}),e.jsx("main",{id:"main-content",className:"pt-16 md:pt-20",children:e.jsx(n,{})}),e.jsx(s,{})]})}export{o as default};
